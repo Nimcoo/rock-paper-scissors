06444ad567e3a18ada628434f40722cce269fdff
NIMCOO <nimco77001@gmail.com>
2023-11-05 14:35:23 +0000

    finished

--- challenge.js
+++ challenge.js
@@ -1,2 +1,10 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+ 
+functionPlaygame(player1,player2)
+if("rock" ,"paper" ){
+return ;"player2 wins"
+ } else if("scissor","scissor")
+ return ;"tie"
+ 
